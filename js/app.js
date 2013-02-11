16fdb60356846eef5a446f94afab2de747de34c4
Mike Ries <michael@riesd.com>
2013-02-11 09:29:47 -0700

    trying to add bootstrap

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.App = Ember.Application.create();
+
+// Router
+// App.Router.map(function() {
+//   this.resource('sections', function() {
+//     this.resource('section', { path: ':table_id' });
+//   });
+// });
+
+// App.IndexRoute = Ember.Route.extend({
+//   redirect: function() {
+//     this.transitionTo('tables');
+//   }
+// });
+
+// App.ApplicationRoute = Ember.Route.extend({
+//   setupController: function() {
+//     this.controllerFor('food').set('model', App.Food.find());
+//   }
+// });
