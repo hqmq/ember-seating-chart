79a4861e044af8476893460de01c59ddd0fd4c36
Michael Ries <michael@riesd.com>
2013-02-12 10:57:15 -0700

    got the seat rendering working in a basic way

--- js/app.js
+++ js/app.js
@@ -51,19 +51,26 @@ App.Event = DS.Model.extend({
     }, 1);
   }.property('registrations.@each.column'),
 
-  registrationsInSection: function(section){
-    this.get('registrations').filter( function(item){
-      return item.get('section') == section;
-    });
-  }.property('registrations.@each.section'),
-
   selectedSection: DS.attr('string'),
   sectionRegistrations: function(){
     var section = this.get('selectedSection');
     return this.get('registrations').filter( function(item){
       return item.get('section') == section;
     });
-  }.property('selectedSection', 'registrations')
+  }.property('selectedSection', 'registrations.@each.seat'),
+
+  seats: function(){
+    var rows = this.get('rows');
+    var columns = this.get('columns');
+    var seats = [];
+    for( var i = 0; i < rows; i++ ){
+      seats.push( [] )
+      for( var j = 0; j < columns; j++) {
+        seats[i].push(null)
+      }
+    }
+    return Ember.ArrayProxy.create({content: seats});
+  }.property('registrations.@each.seat')
 });
 
 App.Registration = DS.Model.extend({
@@ -84,12 +91,20 @@ App.Registration = DS.Model.extend({
 });
 
 App.Event.FIXTURES = [
-  {id: 'MEL121029A', registrations:[1,2,3,4]}
+  {id: 'MEL121029A', registrations:[1,2,3,4,5,6,7,8,9,10,11,12]}
 ]
 
 App.Registration.FIXTURES = [
   {id: 1, seat: 'C1-1', event_id: 'MEL121029A'},
   {id: 2, seat: 'A2-1', event_id: 'MEL121029A'},
   {id: 3, seat: 'A1-2', event_id: 'MEL121029A'},
-  {id: 4, seat: 'A1-3', event_id: 'MEL121029A'}
+  {id: 4, seat: 'A1-3', event_id: 'MEL121029A'},
+  {id: 5, seat: 'A3-3', event_id: 'MEL121029A'},
+  {id: 6, seat: 'D1-1', event_id: 'MEL121029A'},
+  {id: 7, seat: 'D1-2', event_id: 'MEL121029A'},
+  {id: 8, seat: 'D1-3', event_id: 'MEL121029A'},
+  {id: 9, seat: 'B1-1', event_id: 'MEL121029A'},
+  {id: 10, seat: 'B2-3', event_id: 'MEL121029A'},
+  {id: 11, seat: 'B3-3', event_id: 'MEL121029A'},
+  {id: 12, seat: 'B2-2', event_id: 'MEL121029A'}
 ]
