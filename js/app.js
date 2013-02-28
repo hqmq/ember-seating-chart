4e5be0b40f13f789345ad130a1a3d1d234fd44ba
Mike Ries <michael@riesd.com>
2013-02-28 00:18:45 -0700

    adding a modal and sorting the sections

--- js/app.js
+++ js/app.js
@@ -2,25 +2,25 @@ window.App = Ember.Application.create();
 
 //Router
 App.Router.map(function() {
-  this.resource('event');
+  this.resource('chart');
 });
 
 App.IndexRoute = Ember.Route.extend({
   redirect: function() {
-    this.transitionTo('event');
+    this.transitionTo('chart');
   }
 });
 
 App.ApplicationRoute = Ember.Route.extend();
 
-App.EventRoute = Ember.Route.extend({
+App.ChartRoute = Ember.Route.extend({
   model: function(params){
     return App.Event.find(App.event_id);
   }
 });
 
 //Controllers
-App.EventController = Ember.ObjectController.extend({
+App.ChartController = Ember.ObjectController.extend({
   selectSection: function(section){
     this.get('model').set('selectedSection', section);
   }
@@ -35,8 +35,8 @@ App.Event = DS.Model.extend({
   registrations: DS.hasMany('App.Registration'),
   name: DS.attr('string'),
   sections: function(){
-    var arr = this.get('registrations').getEach('section').uniq();
-    return Ember.ArrayProxy.create({content: arr});
+    var arr = this.get('registrations').getEach('section').uniq().sort();
+    return Ember.ArrayProxy.create({content: arr, sortAscending: true});
   }.property('registrations.@each.section'),
 
   rows: function(){
@@ -88,4 +88,55 @@ App.Registration = DS.Model.extend({
   column: function(){
     return this.get('seatParts') ? this.get('seatParts')[3] : 0;
   }.property('seatParts')
+});
+
+App.ModalHeaderView = Ember.View.extend({
+  classNames: ["modal-header"],
+  contentBinding: "parentView.content",
+  titleBinding: "parentView.title",
+  defaultTemplate: Ember.Handlebars.compile("<a href=\"#\" {{action close target=\"view.parentView\"}} class=\"close\">x</a>\n<h3>{{view.title}}</h3>")
+});
+
+App.ModalFooterView = Ember.View.extend({
+  classNames: ["modal-footer"],
+  contentBinding: "parentView.content",
+  defaultTemplate: Ember.Handlebars.compile('the footer')
+});
+
+App.ModalBodyView = Ember.View.extend({
+  classNames: ["modal-body"],
+  contentBinding: "parentView.content",
+  defaultTemplate: Ember.Handlebars.compile('the body')
+});
+
+App.ModalView = Ember.ContainerView.extend({
+  classNames: ["modal"],
+  childViews: (function() {
+    return [this.get('headerView'), this.get('bodyView'), this.get('footerView')].compact();
+  }).property('headerView', 'bodyView', 'footerView'),
+  title: "Default Title",
+  hasCloseButton: true,
+  headerView: App.ModalHeaderView,
+  bodyView: App.ModalBodyView,
+  footerView: null,
+  close: function(e) {
+    //e.preventDefault();
+    return this.destroy();
+  }
+});
+
+App.ModalView.reopenClass({
+  display: function(options) {
+    var modal;
+    if (options == null) {
+      options = {};
+    }
+    modal = this.create(options);
+    modal.append();
+    return modal;
+  }
+});
+
+App.EditModal = App.ModalView.extend({
+
 });
